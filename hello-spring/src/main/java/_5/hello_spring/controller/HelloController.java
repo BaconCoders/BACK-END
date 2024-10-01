5021522268cdcf7ce482ab0e52b06b38f6b56962
mcJo <mung95@naver.com>
2024-10-01 19:36:58 +0900

    test : MVC, API 방식 예제 연습

--- hello-spring/src/main/java/_5/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/_5/hello_spring/controller/HelloController.java
@@ -3,6 +3,8 @@ package _5.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,7 +13,49 @@ public class HelloController {
     public String hello(Model model)
     {
         // http://localhost:8080/hello
+        // http://localhost:8080/hello-static.html
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    //default 값이 value값을 넣어줘야함. 그렇지 않아도 에러가 안나려면 다음과 같이, 필수를 false로 바꿔주면 됨.
+    public String helloMvc(@RequestParam("name") String name, Model model)
+    //public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model)
+    {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name)
+    {
+        return "hello " + name; // "hello spring"
+    }
+
+    static class Hello
+    {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    //객체 반환 시 기본으로 json 으로 반환
+    @GetMapping("hello-api")
+    @ResponseBody // 이런 @붙은걸 애노테이션이라고 함.
+    public Hello helloApi(@RequestParam("name") String name)
+    {
+        //ctrl shift enter 단축키 자동완성
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
 }
